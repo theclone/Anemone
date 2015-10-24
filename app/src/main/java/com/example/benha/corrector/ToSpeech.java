e168b3d226f9ffc8d4e2308440342bc8f0ac92b9
Dan <mrperson7895@gmail.com>
2015-10-24 15:27:12 -0700

    initial text-to-speech code

--- app/src/main/java/com/example/benha/corrector/ToSpeech.java
+++ app/src/main/java/com/example/benha/corrector/ToSpeech.java
@@ -3,6 +3,7 @@ package com.example.benha.corrector;
 import android.app.Activity;
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 import android.view.View;
 
@@ -25,17 +26,41 @@ public class ToSpeech extends TextToSpeech {
         });
     }
 
+    // establish Context to instantiate text to speech
     public static void init(Context context) {
         instance = new ToSpeech(context);
     }
 
-    public void speak() {
+    // loops through the toSpeak queue, speaking each String with a delay in milliseconds
+    public void speak(final int delay) {
         for (String word: toSpeak) {
             speak(word, QUEUE_ADD, null);
-            toSpeak.remove(0);
+            UtteranceProgressListener listener = new UtteranceProgressListener() {
+                @Override
+                public void onStart(String utteranceId) {
+                    Log.d("toSpeak","Playing");
+                }
+
+                @Override
+                public void onDone(String utteranceId) {
+                    Log.d("toSpeak","Done");
+                    toSpeak.remove(0);
+                    try {
+                        Thread.sleep(delay);
+                    } catch (InterruptedException e) {
+                        // carry on
+                    }
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+                    Log.d("toSpeak","RIP");
+                }
+            };
         }
     }
 
+    // to access instance of class
     public static ToSpeech getInstance() {
         return instance;
     }
